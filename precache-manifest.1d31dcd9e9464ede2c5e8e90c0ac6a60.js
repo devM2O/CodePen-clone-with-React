45d24b1d0a16ba84011e2d4e27dc2d32514ead55
minnmawoo <minmawoo.ucsm@gmail.com>
2022-04-24 19:03:09 +0630

    Updates

---
+++ precache-manifest.1d31dcd9e9464ede2c5e8e90c0ac6a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547e2f91c96ae2fb7f6ed4ed20952912",
+    "url": "/CodePen-clone-with-React/index.html"
+  },
+  {
+    "revision": "5b727046daeb525a11d0",
+    "url": "/CodePen-clone-with-React/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "972a74c71c10f361f921",
+    "url": "/CodePen-clone-with-React/static/css/main.9c1638a0.chunk.css"
+  },
+  {
+    "revision": "5b727046daeb525a11d0",
+    "url": "/CodePen-clone-with-React/static/js/2.8ce1cd70.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/CodePen-clone-with-React/static/js/2.8ce1cd70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972a74c71c10f361f921",
+    "url": "/CodePen-clone-with-React/static/js/main.c77200f8.chunk.js"
+  },
+  {
+    "revision": "c27228719316b6829d41",
+    "url": "/CodePen-clone-with-React/static/js/runtime-main.edb2298e.js"
+  }
+]);
